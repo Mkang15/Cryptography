943e36d90c39a24715e1bfed9a63aae96f20e518
noah Branch <nbranch@vols.utk.edu>
2019-09-23 11:52:35 -0400

    this cbc-enc should be able to read your key file and inputfile commandline args

--- cbc-enc.py
+++ cbc-enc.py
@@ -1,7 +1,6 @@
 import argparse
 
-
-if __name__=="__main__":
+def parse_args():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-i","--input",help="input file")
 	parser.add_argument("-o","--output",help="output file")
@@ -9,8 +8,20 @@ if __name__=="__main__":
 	args=parser.parse_args()
 
 	if args.input:
-		print(args.input)
+		inputf = args.input
 	if args.output:
-		print(args.output)
+		outputf = args.output
 	if args.key:
-		print(args.key)
+		keyf = args.key
+	return inputf, outputf, keyf
+
+
+if __name__=="__main__":
+
+	infile,outfile,key = parse_args()
+	
+	f = open(infile,'r')
+	inn = f.read()
+
+	fkey = open(key,'r')
+	keyinn = fkey.read()
